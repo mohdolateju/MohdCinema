bd97202d7f171a9ee722eabc18ad106e63b0dd1b
mohdolateju <mohdolateju@gmail.com>
2013-04-05 22:25:09 +0400

    Trying to restor it

--- application/controllers/CheckOut_Controller.php
+++ application/controllers/CheckOut_Controller.php
@@ -11,13 +11,13 @@ class CheckOut_Controller extends CI_Controller
         /**Default Controller Method*/
        function index(){
            //max number of seat in one cinema
-           //define("MAXSEATNO",30);
+           define("MAXSEATNO",30);
 
            //array to load the selected movies
            $selectedtimes=array();
 
            //resume session and load CheckOut page
-           //session_start();
+           session_start();
 
            //load model
            //$this->load->model("moviedetail_model");
@@ -29,47 +29,47 @@ class CheckOut_Controller extends CI_Controller
 
            $customer_id=$_SESSION['customer_id'];
 
-//           //loop through everything in the sessions
-//           foreach($_SESSION as $cart){
-//               if($cart==$_SESSION['customer_id']||$cart==$_SESSION['type']||$cart==$_SESSION['firstname']
-//                   ||$cart==$_SESSION['lastname']
-//                   ||$cart==$_SESSION['email']||$cart==$_SESSION['username']||$cart==$_SESSION['type']){
-//
-//                   //Do not display the content if is is part of the user information
-//               }
-//               else{
-//                   //get movie id
-//                   $movie_id=$cart['movie_id'];
-//
-//                   //Try to Get the maxScreen No if none exist make it the first one
-//                   if($this->checkout_model->getMaxScreenNo($movie_id)<1){
-//                       $this->screen=1;
-//                   }else{
-//                       //get the screen number from the database if it any exists
-//                       $this->screen=$this->checkout_model->getMaxScreenNo($movie_id);
-//                   }
-//                   //get selected time and add it to the array
-//                   $selectedtime=$cart['selectetime'];
-//                   $selectedtime.=":00";
-//                   $selectedtimes["time$movie_id"]=$selectedtime;
-//                   //Generate voucher
-//                   $voucher=$this->generate_voucher();
-//                   while($this->checkout_model->voucher_exist($voucher)){
-//                       $voucher=$this->generate_voucher();
-//                   }
-//                   //Generate Voucher and seat no and add it the database
-//                   $vouchers["voucher$movie_id"]=$voucher;
-//                   $seat=$this->generate_setno($movie_id,$this->screen);
-//                   $seatNo["seat$movie_id"]=$seat;
-//                   $this->checkout_model->store_voucher($voucher, $movie_id,$customer_id,$seat,$selectedtime,$this->screen);
-//                   $this->screens["screen$movie_id"]=$this->screen;
-//               }
-//           }
+           //loop through everything in the sessions
+           foreach($_SESSION as $cart){
+               if($cart==$_SESSION['customer_id']||$cart==$_SESSION['type']||$cart==$_SESSION['firstname']
+                   ||$cart==$_SESSION['lastname']
+                   ||$cart==$_SESSION['email']||$cart==$_SESSION['username']||$cart==$_SESSION['type']){
+
+                   //Do not display the content if is is part of the user information
+               }
+               else{
+                   //get movie id
+                   $movie_id=$cart['movie_id'];
+
+                   //Try to Get the maxScreen No if none exist make it the first one
+                   if($this->checkout_model->getMaxScreenNo($movie_id)<1){
+                       $this->screen=1;
+                   }else{
+                       //get the screen number from the database if it any exists
+                       $this->screen=$this->checkout_model->getMaxScreenNo($movie_id);
+                   }
+                   //get selected time and add it to the array
+                   $selectedtime=$cart['selectetime'];
+                   $selectedtime.=":00";
+                   $selectedtimes["time$movie_id"]=$selectedtime;
+                   //Generate voucher
+                   $voucher=$this->generate_voucher();
+                   while($this->checkout_model->voucher_exist($voucher)){
+                       $voucher=$this->generate_voucher();
+                   }
+                   //Generate Voucher and seat no and add it the database
+                   $vouchers["voucher$movie_id"]=$voucher;
+                   $seat=$this->generate_setno($movie_id,$this->screen);
+                   $seatNo["seat$movie_id"]=$seat;
+                   $this->checkout_model->store_voucher($voucher, $movie_id,$customer_id,$seat,$selectedtime,$this->screen);
+                   $this->screens["screen$movie_id"]=$this->screen;
+               }
+           }
            //add all generated information to the CheckOut page
-//           $data['selectedtimes']=$selectedtimes;
-//           $data['vouchers']=$vouchers;
-//           $data['seatNo']=$seatNo;
-//           $data['screens']= $this->screens;
+           $data['selectedtimes']=$selectedtimes;
+           $data['vouchers']=$vouchers;
+           $data['seatNo']=$seatNo;
+           $data['screens']= $this->screens;
            $this->load->view("CheckOut");
 
        }
